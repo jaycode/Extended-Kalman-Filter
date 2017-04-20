477677d0891ecccd86211dd9ef709a451a96ec91
Teguh Wijaya <Teguh Wijaya>
2017-04-20 02:05:26 -0700

    init: First commit

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,75 @@
+#include "kalman_filter.h"
+#include <iostream>
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  //cout << "\nF:\n" << F_ << "\n";
+  //cout << "x:\n" << x_ << "\n";
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  //cout << "P:\n" << P_ << "\n";
+  //cout << "Ft:\n" << Ft << "\n";
+  //cout << "Q:\n" << Q_ << "\n";
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  GenericUpdate(y); 
+}
+
+void KalmanFilter::GenericUpdate(const VectorXd &y) {
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+
+  VectorXd hx = VectorXd(3);
+  hx << sqrt(pow(x_[0],2) + pow(x_[1],2)),
+        atan2(x_[1], x_[0]),
+        (x_[0]* x_[2] + x_[1]* x_[3])/sqrt(pow(x_[0],2) + pow(x_[1],2));
+  
+  VectorXd y = z - hx;
+  //cout << "y:\n" << y << "\n";
+  GenericUpdate(y);
+}
---
+++ src/tools.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+using namespace std;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+	const vector<VectorXd> &ground_truth) {
+
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	// ... your code here
+	if (estimations.size() == 0) {
+		cout << "CalculateRMSE() - Error - estimations size should not be zero.";
+	}
+	else {
+		if (estimations.size() != ground_truth.size()) {
+			cout << "CalculateRMSE() - Error - Size of estimations and ground_truth should be equal.";
+		}
+	}
+
+	//accumulate squared residuals
+	for (int i = 0; i < estimations.size(); ++i) {
+		VectorXd errors = estimations[i] - ground_truth[i];
+		errors = errors.array()*errors.array();
+		rmse += errors;
+	}
+
+	//calculate the mean
+	rmse /= estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+	MatrixXd Hj(3, 4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//check division by zero
+	if (px == 0 || py == 0) {
+		cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+    return Hj;
+	}
+
+  // pre-compute
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+  // c3 is equal to pow((pow(px, 2) + pow(py, 2)), 3 / 2);
+
+	//compute the Jacobian matrix
+	float j1 = px / c2;
+	float j2 = py / c2;
+	float j3 = 0;
+	float j4 = 0;
+	float j5 = -py / c1;
+	float j6 = px / c1;
+	float j7 = 0;
+	float j8 = 0;
+  float j9 = (py*(vx*py - vy*px)) / c3;
+	float j10 = (px*(vy*px - vx*py)) / c3;
+	float j11 = px / c2;
+	float j12 = py / c2;
+
+	Hj << j1, j2, j3, j4,
+		j5, j6, j7, j8,
+		j9, j10, j11, j12;
+	return Hj;
+}
